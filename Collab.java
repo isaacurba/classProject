56c96ad0189f15df3d3a8082d0a59831b7d70269
Ovie19 <adetolabenjamin18@gmail.com>
2026-08-25 09:23:00 +0100

    new message

--- Collab.java
+++ Collab.java
@@ -1,7 +1,7 @@
 public class Collab{
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world from Nicolas Jackson");
     }
 
 }
